4631f18db798c2dbe8db0f377954f045c703202c
junsennn123 <ongjunsen@gmail.com>
2021-03-18 03:33:14 +0800

    trying to get nearby schl done but fk man

--- SchoolConnect/app/src/main/java/com/ce2006/schoolconnect/GPSActivity.java
+++ SchoolConnect/app/src/main/java/com/ce2006/schoolconnect/GPSActivity.java
@@ -1,20 +1,55 @@
 package com.ce2006.schoolconnect;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
  */
-public class GPSActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class GPSActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleMap mMap;
+    LocationRequest request;
+    GoogleApiClient client;
+    LatLng latLngCurrent;
+    //EditText el;
+
+    double currentLat;
+    double currentLng;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mGetedLocation;
+    public static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +72,116 @@ public class GPSActivity extends AppCompatActivity implements OnMapReadyCallback
      * Play services inside the SupportMapFragment. The API invokes this method after the user has
      * installed Google Play services and returned to the app.
      */
+
+    public void findSchools(View v) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+            return;
+        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(GPSActivity.this);
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            mGetedLocation = task.getResult();
+                            currentLat = mGetedLocation.getLatitude();
+                            currentLng = mGetedLocation.getLongitude();
+                            //updateUI();
+                        } else {
+                            //Log.e(TAG, "no location detected");
+                            //Log.w(TAG, "getLastLocation:exception", task.getException());
+                        }
+                    }
+                });
+
+
+        StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        stringBuilder.append("location=" + currentLat + "," + currentLng);
+        stringBuilder.append("&radius=" + 1000);
+        stringBuilder.append("&keyword=" + "school");
+        stringBuilder.append("&key=" + "AIzaSyAVTbpSW_XBVvjRvSXddw0kfQmRJy_4hHI");
+
+        String url = stringBuilder.toString();
+
+        Object dataTransfer[] = new Object[2];
+        dataTransfer[0] = mMap;
+        dataTransfer[1] = url;
+
+        GetNearbyPlaces getNearbyPlaces = new GetNearbyPlaces(this);
+        getNearbyPlaces.execute(dataTransfer);
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng ntu = new LatLng(User.getLat(), User.getLong());
+        /*LatLng ntu = new LatLng(User.getLat(), User.getLong());
         googleMap.addMarker(new MarkerOptions()
                 .position(ntu)
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(ntu));
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(16));*/
+
+        mMap = googleMap;
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        client.connect();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location)
+    {
+        if(location == null)
+        {
+            Toast.makeText(getApplication(), "Location not found." , Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            latLngCurrent = new LatLng(location.getLatitude(),location.getLongitude());
+
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLngCurrent,15);
+            mMap.animateCamera(update);
+
+            MarkerOptions options = new MarkerOptions();
+            options.position(latLngCurrent);
+            options.title("Current location");
+            mMap.addMarker(options);
+
+        }
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        request = new LocationRequest().create();
+        request.setInterval(1000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        //LocationServices.FusedLocationApi.requestLocationUpdates(client, request, (com.google.android.gms.location.LocationListener) this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
---
+++ SchoolConnect/app/src/main/java/com/ce2006/schoolconnect/GetNearbyPlaces.java
@@ -0,0 +1,117 @@
+package com.ce2006.schoolconnect;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GetNearbyPlaces extends AsyncTask<Object,String, String>
+{
+
+    GoogleMap mMap;
+    String url;
+    InputStream is;
+    BufferedReader bufferedReader;
+    StringBuilder stringBuilder;
+    String data;
+    Context context;
+
+    public GetNearbyPlaces(Context context)
+    {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Object... params) {
+
+        mMap = (GoogleMap)params[0];
+        url = (String) params[1];
+
+        try
+        {
+            URL myurl = new URL(url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)myurl.openConnection();
+
+            httpURLConnection.connect();
+
+            is = httpURLConnection.getInputStream();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+            String line = "";
+            stringBuilder = new StringBuilder();
+            while((line = bufferedReader.readLine()) != null)
+            {
+                stringBuilder.append(line);
+            }
+
+            data = stringBuilder.toString();
+
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        //return null;
+
+        System.out.println("Enter post execute for get places");
+
+        try
+        {
+            JSONObject parentObject = new JSONObject(s);
+            JSONArray resultsArray = parentObject.getJSONArray("results");
+
+            for(int i = 0 ; i < resultsArray.length(); i++)
+            {
+                JSONObject jsonObject = resultsArray.getJSONObject(i);
+                JSONObject locationObj = jsonObject.getJSONObject("geometry").getJSONObject("location");
+
+                String latitude = locationObj.getString("lat");
+                String longitude = locationObj.getString("lng");
+
+                JSONObject nameObject = resultsArray.getJSONObject(i);
+
+                String name_school = nameObject.getString("name");
+                String vicinity = nameObject.getString("vicinity");
+
+                LatLng latLng = new LatLng(Double.parseDouble(latitude),Double.parseDouble(longitude));
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.title(vicinity);
+                markerOptions.position(latLng);
+
+                mMap.addMarker(markerOptions);
+
+            }
+
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+}
